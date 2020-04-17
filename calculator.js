504fbe34518747c2e78f58b4074991112948051e
Mike <m_kanyar@encs.concordia.ca>
2020-04-16 20:18:14 -0400

    Adding the form of the calculator

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
-  res.send("Hello world");
+  res.sendFile(__dirname + "/index.html");
 });
 app.listen(3000, function () {
   console.log("This is a calculator application");
